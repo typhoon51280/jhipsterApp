9fc01cf23162285af96948b7eacad04e59fb1d63
Vincenzo Dell'Oste <vash51280@gmail.com>
2018-04-19 06:25:34 +0000

    entity model

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,16 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { JhipsterAppNazioneModule } from './nazione/nazione.module';
+import { JhipsterAppRegioneModule } from './regione/regione.module';
+import { JhipsterAppProvinciaModule } from './provincia/provincia.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
   // prettier-ignore
   imports: [
+        JhipsterAppNazioneModule,
+        JhipsterAppRegioneModule,
+        JhipsterAppProvinciaModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
   declarations: [],
